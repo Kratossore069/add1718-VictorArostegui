611817419ad0a15b10f6771af33b913d488753d3
VictorArostegui <victorgabrielarostegui@gmail.com>
2018-02-06 10:41:50 +0000

    Calculadora1

---
+++ trim2/Calculadora/cal1.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+num1 = ARGV[0].to_i
+op = ARGV[1]
+num2 = ARGV[2].to_i
+
+if ARGV.size == 3
+
+  if op == '+'
+    suma = num1 + num2
+    puts "El resultado es #{suma}"
+
+  elsif op == '-'
+    resta = num1 - num2
+    puts "El resultado es #{resta}"
+
+  elsif op == 'x'
+    multiplicar = num1 * num2
+    puts "El resultado es #{multiplicar}"
+
+  elsif op == '/'
+    dividir = num1 / num2
+    puts "El resultado es #{dividir}"
+
+  end
+
+end
+
+
