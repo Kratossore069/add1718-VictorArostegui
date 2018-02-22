27f4d014dc6c0d35f9e4c788d34d799ce53bdd90
VictorArostegui <victorgabrielarostegui@gmail.com>
2018-02-22 11:51:59 +0000

    Delete3

--- trim2/Crear/create-and-delete-users.rb
+++ trim2/Crear/create-and-delete-users.rb
@@ -1,9 +1,33 @@
 #!/usr/bin/ruby
 
 admin = `whoami`
-if admin != 'root'
-  puts("Debe ser el usuario root para acceder a este script.")
+
+if admin != "root"
+  puts "Debe ser el usuario root para acceder a este script."
   exit
-end 
+  
+  
+elsif admin == "root"
+  file = `cat userslist.txt`
+  file = file.split("\n")
+  
+  
+  if file[4] == "delete"
+    eliminar = `rm userslist.txt`
+    puts "#{eliminar}"
+    
+    
+  elsif file[4] == "add"
+    crear = `create user victorcarvajal`
+    puts "#{crear}"
+    
+  else file[2] != "victor@gmail.com"
+    puts "Este usuario no tiene establecido un email."
+  end
+  
+end
+
+
+   
 
 
