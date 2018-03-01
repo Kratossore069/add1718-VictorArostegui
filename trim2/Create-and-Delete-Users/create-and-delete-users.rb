bffc47a393b26be38ccce8a6496a6ccedb0172fe
VictorArostegui <victorgabrielarostegui@gmail.com>
2018-03-01 10:40:32 +0000

    CrearyEliminar2

--- trim2/Create-and-Delete-Users/create-and-delete-users.rb
+++ trim2/Create-and-Delete-Users/create-and-delete-users.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/ruby
 
+file = ARGV[0]
+usuario = `cat #{file}`
+usuario = usuario.split("\n")
+system("cat #{usuario}")
 
+user = `whoami`
+
+if user != "root"
+  then
+    puts "No puede ejecutar este script debido a que no es el usuario root."
+  else
+    puts "Usuario root confirmado."
+end
