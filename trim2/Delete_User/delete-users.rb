474922284d3c332858e3661a679083a43f636f2a
VictorArostegui <victorgabrielarostegui@gmail.com>
2018-03-07 11:13:20 +0000

    Final

--- trim2/Delete_User/delete-users.rb
+++ trim2/Delete_User/delete-users.rb
@@ -5,13 +5,19 @@ fichero = `cat #{documento}`  				# Creo una variable para ese argumento.
 fich = fichero.split("\n")  				# Con esa variable que contiene el argumento, elimino el último espacio en blanco.
 puts "El contenido del fichero es #{fich}"
 
-fich.each do |user|  
-  if user != ""
-    then
+if ARGV.empty?								# Pregunto si hay un archivo asociado al script.
+  puts "No ha especificado un archivo."
+  exit
+end
+
+fich.each do |user|  						# Para cada "caja" dentro de ese archivo.
+  existe = `id #{user}`						# Valor de la id del usuario.
+  if existe != ""	then					# Si el espacio es distinto de que esté en blanco...
+	  puts "Se ha encontrado el usuario #{user}"
       system("sudo userdel -r #{user}")
       puts "Se ha eliminado el usuario #{user}"
-    else
-      puts "El usuario #{user} ya se encuentra eliminado."
+    else									# Si el espacio está en blanco, significa que ya se eliminó ese usuario.
+      puts "El usuario #{user} no existe."
   end
 end
   
